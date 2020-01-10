5fb757c124cccbf7da7737612356bbe237c3014a
Nathanael Tan <nathanael.tan92@gmail.com>
2020-01-10 14:53:44 +0800

    main logic for hierarchy done using recursive query

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Employee.create(name: 'John', manager_id: 2)
+# Employee.create(name: 'Tom', manager_id: 3)
+Employee.create(name: 'Harry')
+
